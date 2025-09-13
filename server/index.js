e03624e17f2b58f03631240fdc18510394c38492
Shubham Yadav <yadavshubham6.022@gmail.com>
2025-09-13 11:42:06 +0530

    add frountend url in cors

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ connectDB();
 
 app.use(
   cors({
-    origin:"*",
+    origin:"https://usaan.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
